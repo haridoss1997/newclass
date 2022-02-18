c30fd3e6fd7dea4bcf89d61a2ffde6d6d138c219
haridoss1997 <haridoss1997@gmail.com>
2022-02-18 16:23:41 +0530

    HARI1

--- src/org/samble/javasamble.java
+++ src/org/samble/javasamble.java
@@ -3,7 +3,16 @@ package org.samble;
 public class javasamble {
 	private void employee() {
 		System.out.println("hari");
+	}
+	
+	private void employeeid() {
+		System.out.println("234");
 
 	}
 
+	public static void main(String[] args) {
+		javasamble java = new javasamble();
+		java.employee();
+		java.employeeid();
+	}
 }
